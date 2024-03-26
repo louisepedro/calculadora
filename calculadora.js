0713201b040073384c0965c69941921e673a3f4e
Auto Logon <autologon@CSED.LAB>
2024-03-26 10:12:08 -0300

    Calculadora

---
+++ calculadora.js
@@ -0,0 +1,16 @@
+var somar = require('./somar');
+var subtrair = require('./subtrair');
+var  multiplicar = require("./multiplicar");
+var dividir = require('./dividir');
+
+console.log("adição");
+console.log(somar(17,9));
+
+console.log("Suntração");
+console.log(subtrair(17,9));
+
+console.log("MUltiplicação");
+console.log(multiplicar(17,9));
+
+console.log("divisão")
+console.log(dividir(17,9));
